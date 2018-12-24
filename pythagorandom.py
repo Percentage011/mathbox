3cc70d3fbde8e92b926937891cba17af7f83f784
Percentage011 <43078231+Percentage011@users.noreply.github.com>
2018-12-25 04:00:49 +0900

    Update pythagorandom.py

--- pythagorandom.py
+++ pythagorandom.py
@@ -1,14 +1,13 @@
 import math,random
 
-x=random.randrange(1,7)
-a=random.randrange(1,10**x,2)
-b=random.randrange(2,10**x+1,2)
+x=random.randrange(1,7) #乱数の分布幅をランダムに決める
+a=random.randrange(1,10**x,2) #奇数をランダムに生成
+b=random.randrange(2,10**x+1,2) #偶数をランダムに生成
 
-n=a**2+b**2
+n=a**2+b**2 #分解される形にする(必ずしも原始ピタゴラス数の組があるとは限らない)
 s=n #nを保存しておく
 
 p=[] #ピタゴラス数の組を入れておくリストのリスト。これ自体の要素はリスト
-#4で割ったあまりが1の場合の処理。ピタゴラス数に分解できる可能性がある
 m=1 #mの初期化
 i=-1 #リストpのインデックス用のカウンタ変数。c-m*mが平方数の時に働く
 while((m*m)<n): #それぞれのmに対してcが平方数であるかを確認する
