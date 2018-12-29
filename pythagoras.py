fe187e2789f57054dc1d6dd8f125e60fca6bc46b
Percentage <43078231+Percentage011@users.noreply.github.com>
2018-12-30 07:54:03 +0900

    Update pythagoras.py

--- pythagoras.py
+++ pythagoras.py
@@ -3,40 +3,38 @@ n=int(N)
 
 import math
 
-#入力を原始ピタゴラス数に揃えるためにできるだけ2で割り、2で割ったあまりが1であるか判定する
+#入力を原始ピタゴラス数に揃えるためにできるだけ4で割り、4で割ったあまりが1であるか判定する
 k=0
 while(n%2==0):
     n=n/2
     k+=1
-    continue
 
 p=[] #ピタゴラス数の組を入れておくリストのリスト。これ自体の要素はリスト
 #4で割ったあまりが1の場合の処理。ピタゴラス数に分解できる可能性がある
 if(n%4==1 and n!=1):
-        m=1 #mの初期化
-        i=-1 #リストpのインデックス用のカウンタ変数。c-m*mが平方数の時に働く
-        while((m*m)<n): #それぞれのmに対してcが平方数であるかを確認する
-            c=n #毎回初期化する
-            c=abs(c-(m*m))
-            m+=1
-            if(math.sqrt(c)%1==0): #cが平方数のときに以下の処理をする
-                i+=1
-                p.append([])
-                q=abs(c-((m-1)*(m-1)))
-                t=(2*(m-1)*(math.sqrt(c)))
-                if(k!=0):
-                    q=q*(2**k)
-                    t=t*(2**k)
-
-                c=int(c)
-                t=int(t)
-                q=int(q)
-                p[i].append(t)
-                p[i].append(q)
+    a=1 #aの初期化
+    i=-1 #リストpのインデックス用のカウンタ変数。c*c-a*aが平方数の時に働く
+    while(a<n/math.sqrt(2)): #それぞれのaに対してbbが平方数であるかを確認する
+        c=n #毎回初期化する
+        bb=c*c-a*a
+        a+=1
+        if(math.sqrt(bb)%1==0): #bbが平方数のときに以下の処理をする
+            i+=1
+            a-=1
+            b=math.sqrt(bb)
+            p.append([])
+            a=a*(2**k)
+            b=b*(2**k)
+            a=int(a)
+            b=int(b)
+            p[i].append(a)
+            p[i].append(b)
+            a=a/(2**k)
+            a+=1
 
 if(len(p)!=0):
     print("できました！")
-    for r in range(int(len(p)/2)):
+    for r in range(len(p)):
         text=str(p[r])
         if(math.gcd(p[r][0],p[r][1])==1):
             text=text+"　←これは原始ピタゴラス数だよ！"
